ae9789a3c023ac5c9550daf91eb1fbba6a843bc4
Ryoh Akiyoshi <ryo614@gmail.com>
2022-02-23 23:09:30 +0900

    [2-4] Abstract syntax tree

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -16,6 +16,7 @@ let foobar = 838383;
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -56,11 +57,23 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
 	}
 
-	if letStmt.Name.TokenLiteral() != Name {
+	if letStmt.Name.TokenLiteral() != name {
 		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s",
 			name, letStmt.Name.TokenLiteral())
-		return fasle
+		return false
 	}
 
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
