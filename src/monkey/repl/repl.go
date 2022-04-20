356de519c8885d3e3e212696377ad9edcea6fd1a
Ryoh Akiyoshi <ryo614@gmail.com>
2022-04-20 22:51:25 +0900

    parse and print

--- src/monkey/repl/repl.go
+++ src/monkey/repl/repl.go
@@ -5,10 +5,19 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
 const PROMPT = ">> "
+
+const KABAO = `      ∩＿∩
+   ／ ＼ ／＼
+  |（ﾟ）=（ﾟ）|
+  |   ●_●  |
+ /           ヽ
+ | 〃 ------ ヾ
+ ＼＿＿二＿＿ノ
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -22,9 +31,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, KABAO)
+	io.WriteString(out, "すごい一体感を感じる。今までにない何か熱い一体感を。\n")
+	io.WriteString(out, "parser errors: \n ")
+	for _, msg := range errors {
+		io.WriteString(out, fmt.Sprintf("\t%s\n", msg))
 	}
 }
