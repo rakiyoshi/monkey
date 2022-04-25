2bc0ceda57d53d7b055af242bbc0d10c851fd110
Ryoh Akiyoshi <ryo614@gmail.com>
2022-04-25 23:26:06 +0900

    [3.5.5] add prefix expression evaluator

--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -14,6 +14,8 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
 	}
 
 	for _, tt := range tests {
@@ -23,18 +25,37 @@ func TestEvalIntegerExpression(t *testing.T) {
 }
 
 func TestEvalBooleanExpression(t *testing.T) {
-    tests := []struct {
-        input string
-        expected bool
-    }{
-        {"true", true},
-        {"false", false},
-    }
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
 
-    for _, tt := range tests {
-        evaluated := testEval(tt.input)
-        testBooleanObject(t, evaluated, tt.expected)
-    }
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
 }
 
 // helper functions
@@ -57,14 +78,14 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 }
 
 func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
-    result, ok := obj.(*object.Boolean)
-    if !ok {
-        t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
-        return false
-    }
-    if result.Value != expected {
-        t.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
-        return false
-    }
-    return true
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
+		return false
+	}
+	return true
 }
