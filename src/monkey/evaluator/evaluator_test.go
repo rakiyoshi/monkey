aa358e735f280d0ace0352202ab7c8ab0e9cd9ed
Ryoh Akiyoshi <ryo614@gmail.com>
2022-04-30 19:31:33 +0900

    [4.2.3] add evaluator for string

--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -257,6 +257,20 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestStringLiteral(t *testing.T) {
+	input := `"Hello World!";`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
 // helper functions
 func testEval(input string) object.Object {
 	l := lexer.New(input)
