2f055b658de021aa577bbe02e6d8fb766e09ad42
Ryoh Akiyoshi <ryo614@gmail.com>
2022-05-07 16:47:47 +0900

    [4.5.1] add lexer for hash

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -29,6 +29,7 @@ if (5 < 10) {
 "foobar"
 "foo bar"
 [1, 2];
+{"foo": "bar"}
 `
 
 	tests := []struct {
@@ -134,6 +135,12 @@ if (5 < 10) {
 		{token.INT, "2"},
 		{token.RBRACKET, "]"},
 		{token.SEMICOLON, ";"},
+		// {"foo": "bar"}
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
 		// EOF
 		{token.EOF, ""},
 	}
