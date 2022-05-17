556be360a5c73f8c18bee81470bd90960c2464e7
Ryoh Akiyoshi <ryo614@gmail.com>
2022-05-17 23:07:41 +0900

    [A.5.1] add lexer for macro

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -30,6 +30,7 @@ if (5 < 10) {
 "foo bar"
 [1, 2];
 {"foo": "bar"}
+macro(x, y) { x + y; };
 `
 
 	tests := []struct {
@@ -141,6 +142,20 @@ if (5 < 10) {
 		{token.COLON, ":"},
 		{token.STRING, "bar"},
 		{token.RBRACE, "}"},
+		// macro(x, y) { x + y; };
+		{token.MACRO, "macro"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
 		// EOF
 		{token.EOF, ""},
 	}
